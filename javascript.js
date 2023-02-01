2fffff5cf2e3819c15128262c5b849cfc39750c8
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-02-01 14:10:03 +0100

    Add reset grid functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,27 @@
 const gridContainer = document.querySelector('.grid-container');
+const resetBtn = document.querySelector('.input-container button');
+const userInput = document.getElementById('grid-size');
 
 createGrid(16);
+
+resetBtn.addEventListener('click', e => {
+    if(userInput.valueAsNumber < 16 || userInput.valueAsNumber > 100 || Number.isNaN(userInput.valueAsNumber)){
+        userInput.style.backgroundColor = "red";
+    } else{
+        userInput.style.backgroundColor = "white";
+        resetGrid();
+    }
+});
+
+function resetGrid(){
+    let gridCols = document.querySelectorAll('.grid-col');
+
+    gridCols.forEach(el => {
+        gridContainer.removeChild(el);
+    });
+
+    createGrid(userInput.valueAsNumber);
+}
 
 function createGrid(n){
     for(let i=0; i<n; i++){
